5b4167ea42394b4cc4779318196fe0ecf39b57e8
msy1332 <17322714918@163.com>
2025-03-27 18:17:22 +0800

    Completed the button add key message

--- src/SnackMain.cpp
+++ src/SnackMain.cpp
@@ -19,47 +19,53 @@ int main()
     loadimage(&BackDrop_Img,"../resources/Image/SnackBackdrop.jpg",844,500);
     putimage(0,0,&BackDrop_Img);
 
-    IMAGE PlayText;
-    IMAGE PlayText1;
-    loadimage(&PlayText,"../resources/Image//Text.png");
-    loadimage(&PlayText1,"../resources/Image//Text.png",200*1.1,80*1.1);
-
-    int Play_X=324;
-    int play_Y=350;
-    int PlayText_Width=PlayText.getwidth();
-    int PlayText_Height=PlayText.getheight();
-
-    PutImage putimage;
-    putimage.Transparency_Map(324,350,&PlayText);
-    //putimage.Transparency_Map(312,350,&PlayText1);
+    //加载按钮
+    PutImage put_image;
+    IMAGE Button1;
+    IMAGE Button2;
+    IMAGE Button3;
     int PlayText_Status=Normal;
+    loadimage(&Button1,"../resources/Image/Button1.png",707/4,249/4);
+    loadimage(&Button2,"../resources/Image/Button2.png",707/4,249/4);
+    loadimage(&Button3,"../resources/Image/Button3.png",707/4,249/4);
 
+    int Button_X=344;
+    int Button_Y=380;
+    int Button_Width=Button1.getwidth();
+    int Button_Height=Button1.getheight(); 
+
+    //定义鼠标消息
     ExMessage msg;
 
-    bool Game_Status=true;
-    bool Game_Start=false;
+    bool Game_Status=true;//游戏的运行状态
+    bool Game_Start=false;//游戏的开始状态
+
+    //游戏的主循环
     while(Game_Status)
     {
+        //获取鼠标消息
         if(peekmessage(&msg,EX_MOUSE|EX_KEY))
         {
-            if(Game_Start)
+            //判断游戏是否开始
+            if(!Game_Start)
             {
                 switch(msg.message)
                 {
                     case WM_LBUTTONDOWN:
-                        if(msg.x>Play_Y&&msg.x<Play_X+PlayText_Width&&msg.y>Play_Y&&msg.y<Play_Y+PlayText_Height)
+                        if(msg.x>Button_X&&msg.x<Button_X+Button_Width&&msg.y>Button_Y&&msg.y<Button_Y+Button_Height)
                         {
                             PlayText_Status=Pressed;
                         }
                         break;
                     case WM_LBUTTONUP:
-                        if(msg.x>Play_Y&&msg.x<Play_X+PlayText_Width&&msg.y>Play_Y&&msg.y<Play_Y+PlayText_Height)
+                        if(msg.x>Button_X&&msg.x<Button_X+Button_Width&&msg.y>Button_Y&&msg.y<Button_Y+Button_Height)
                         {
+                            Game_Start=true;
                             PlayText_Status=Hover;
                         }
                         break;
                     default:
-                        if(msg.x>Play_Y&&msg.x<Play_X+PlayText_Width&&msg.y>Play_Y&&msg.y<Play_Y+PlayText_Height)
+                        if(msg.x>Button_X&&msg.x<Button_X+Button_Width&&msg.y>Button_Y&&msg.y<Button_Y+Button_Height)
                         {
                             PlayText_Status=Hover;
                         }
@@ -72,19 +78,83 @@ int main()
             }
             else
             {
+                //获取按键消息
                 switch(msg.message)
                 {
-                    case WM_KEYDOWN:
+                    case WM_KEYDOWN://按键按下消息
+                        switch(msg.vkcode)
+                        {
+                            case 'w':
+                            case 'W':
+                                cout<<"W Press"<<endl;
+                                break;
+                            case 's':
+                            case 'S':
+                                cout<<"S Press"<<endl;
+                                break;
+                            case 'd':
+                            case 'D':
+                                cout<<"D Press"<<endl;
+                                break;
+                            case 'n':
+                            case 'N':
+                                cout<<"N Press"<<endl;
+                                break;
+                        }
                         break;
-                    case WM_KEYUP:
+                    case WM_KEYUP://按键弹起消息
+                    switch(msg.vkcode)
+                    {
+                        case 'w':
+                        case 'W':
+                            cout<<"W Bounce UP"<<endl;
+                            break;
+                        case 's':
+                        case 'S':
+                            cout<<"S Bounce UP"<<endl;
+                            break;
+                        case 'd':
+                        case 'D':
+                            cout<<"D Bounce UP"<<endl;
+                            break;
+                        case 'n':
+                        case 'N':
+                            cout<<"N Bounce UP"<<endl;
+                            break;
+                    }
                         break;
                 }
             }
         }
+        BeginBatchDraw();//开始双缓冲绘图
+        cleardevice();//清屏
+        if(!Game_Start)//判断游戏是否开始游戏
+        {
+            if(PlayText_Status==Pressed)
+            {
+                putimage(0,0,&BackDrop_Img);
+                put_image.Transparency_Map(344,380,&Button3);
+            }
+            else if(PlayText_Status==Hover)
+            {
+                putimage(0,0,&BackDrop_Img);
+                put_image.Transparency_Map(344,380,&Button1);
+            }
+            else
+            {
+                putimage(0,0,&BackDrop_Img);
+                put_image.Transparency_Map(344,380,&Button2);
+            }
+        }
+        else
+        {
+            
+        }
+        EndBatchDraw();//结束双缓冲绘图
     }
 
-    system("pause");
-    closegraph();
+    system("pause");//暂停窗口
+    closegraph();//关闭窗口
     return 0;
 }
 /*
